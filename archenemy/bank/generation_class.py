15285cdf273b99ccc7a42317502a7443cd3ec44a
ArthurTolley <arthur.tolley@port.ac.uk>
2023-02-01 11:24:05 +0000

    Improving the efficiency of the bank generation code

--- archenemy/bank/generation_class.py
+++ archenemy/bank/generation_class.py
@@ -1,38 +1,32 @@
-from pathlib import Path
-import sys
-import os
-p = str(Path(os.path.abspath('')).parents[0])
-sys.path.insert(1, p)
+import numpy as np
+import logging
+from typing import Union, List
+import math
 
 import pycbc
 import pycbc.psd
-import logging
-from typing import Union
-import math
-from removal.removal_class import ArtefactGeneration
 from pycbc.psd import interpolate
-import numpy as np
 from pycbc.filter import make_frequency_series, match
 from pycbc.types.timeseries import TimeSeries, FrequencySeries
 
+from search.artefact_template import ArtefactGeneration
+
 class GenerateTemplateBank:
     def __init__(self,
-                 fringe_frequency_lower: float = None,
-                 fringe_frequency_upper: float = None,
-                 timeperiod_lower: float = None,
-                 timeperiod_upper: float = None,
+                 glitch_frequency_range: List[float, float] = None,
+                 timeperiod_range: List[float, float] = None,
                  match_limit: float = None,
                  maximum_templates: float = None,
-                 psd: Union[FrequencySeries, str]  = None,
+                 psd: FrequencySeries = None,
                  sample_rate: float = 2048.0,
                  f_low: float = 15.0,
                  find_template_splits: bool = False):
 
-        self.fringe_frequency_lower = fringe_frequency_lower
-        self.fringe_frequency_upper = fringe_frequency_upper
+        self.glitch_frequency_lower = glitch_frequency_range[0]
+        self.glitch_frequency_upper = glitch_frequency_range[0]
 
-        self.timeperiod_lower = timeperiod_lower
-        self.timeperiod_upper = timeperiod_upper
+        self.timeperiod_lower = timeperiod_range[0]
+        self.timeperiod_upper = timeperiod_range[1]
 
         self.match_limit = match_limit
         self.maximum_templates = maximum_templates
@@ -50,52 +44,25 @@ class GenerateTemplateBank:
 
         self.f_low = f_low
         self.psd = psd
-        if not isinstance(self.psd, FrequencySeries):
-            self.generate_psd()
 
-    def generate_psd(self):
-        try:
-            tlen = int(self.timeperiod_upper * self.sample_rate)
-            f_low = 15.0
-            delta_f = 1.0 / self.timeperiod_upper
-            flen = int(tlen//2 + 1)
-            self.psd = pycbc.psd.from_string(self.psd, flen, delta_f, f_low)
-        except:
-            print('PSD not generated correctly.')
-
-    def generate_template(self,
-                          fringe_frequency: float,
-                          timeperiod: float):
-        data_time_length = self.timeperiod_upper
-        gen_template = ArtefactGeneration(start_time = 0,
-                                    data_time_length = data_time_length,
-                                    fringe_frequency = fringe_frequency,
-                                    timeperiod = timeperiod,
-                                    amplitude = 1e-21,
-                                    phase = 0.0,
-                                    pad = True,
-                                    sample_rate = self.sample_rate,
-                                    psd = self.psd,
-                                    roll = False)
-        template = gen_template.generate_template()
-
-        return template
-
-    def calculate_template_splits(self,
-                                  fringe_frequency: float,
-                                  timeperiod: float):
-
-        gen_template = ArtefactGeneration(start_time = 0,
-                                          data_time_length = None,
-                                          fringe_frequency = fringe_frequency,
-                                          timeperiod = timeperiod,
+    def generate_template(self):
+        duration = self.timeperiod_upper
+        gen_template = ArtefactGeneration(fringe_frequency = self.glitch_frequency,
+                                          timeperiod = self.timeperiod,
                                           amplitude = 1e-21,
                                           phase = 0.0,
-                                          pad = False,
-                                          sample_rate = self.sample_rate,
-                                          psd = self.psd)
-        self.template = gen_template.generate_template()
-        fs_template = make_frequency_series(self.template)
+                                          sample_rate = self.sample_rate)
+
+        gen_template.generate_template(duration = duration,
+                                       start_time = 0.0,
+                                       pad = True,
+                                       make_timeseries=True,
+                                       make_frequency_series=True)
+
+        self.ts_template = gen_template.template
+        self.fs_template = gen_template.fs_template
+
+    def calculate_template_splits(self):
 
         interpolated_psd = interpolate(self.psd, self.template.delta_f)
 
@@ -104,7 +71,7 @@ class GenerateTemplateBank:
 
         cut_idx = self.f_low * self.template.duration
         interpolated_psd = interpolated_psd[int(cut_idx+1):-1]
-        fs_template = fs_template[int(cut_idx+1):-1]
+        fs_template = self.fs_template[int(cut_idx+1):-1]
         self.white_temp = (fs_template / interpolated_psd**0.5).to_timeseries()
 
         cumsum = np.cumsum((self.white_temp*self.white_temp))
@@ -115,10 +82,10 @@ class GenerateTemplateBank:
         idx_3 = idx_2 + (np.abs(cumsum[idx_2:] - ex_frac*3)).argmin()
         idx_4 = len(cumsum) - idx_3
 
-        self.frac_1 = (idx)/(timeperiod * self.sample_rate)
-        self.frac_2 = (idx_2)/(timeperiod * self.sample_rate) - self.frac_1
-        self.frac_3 = (idx_3)/(timeperiod * self.sample_rate) - self.frac_2 - self.frac_1
-        self.frac_4 = (idx_4)/(timeperiod * self.sample_rate)
+        self.frac_1 = (idx)/(self.timeperiod * self.sample_rate)
+        self.frac_2 = (idx_2)/(self.timeperiod * self.sample_rate) - self.frac_1
+        self.frac_3 = (idx_3)/(self.timeperiod * self.sample_rate) - self.frac_2 - self.frac_1
+        self.frac_4 = (idx_4)/(self.timeperiod * self.sample_rate)
 
     def match_template_with_bank(self,
                                  template):
@@ -145,20 +112,20 @@ class GenerateTemplateBank:
         return True
 
     def generate_template_bank(self):
-        first_template = (np.random.uniform(low=self.fringe_frequency_lower, high=self.fringe_frequency_upper),
-                          np.random.uniform(low=self.timeperiod_lower, high=self.timeperiod_upper))
-        first_ts = self.generate_template(first_template[0], first_template[1])
-        first_fs = make_frequency_series(first_ts)
-        first_template = (first_template[0],
-                          first_template[1],
-                          first_fs)
+        self.glitch_frequency = np.random.uniform(low=self.glitch_frequency_lower,
+                                                  high=self.glitch_frequency_upper)
+        self.timeperiod = np.random.uniform(low=self.timeperiod_lower,
+                                            high=self.timeperiod_upper)
+        self.generate_template()
+        first_template = (self.glitch_frequency,
+                          self.timeperiod,
+                          self.fs_template)
 
         if self.find_template_splits is True:
-            self.calculate_template_splits(first_template[0],
-                                           first_template[1])
-            first_template = (first_template[0],
-                              first_template[1],
-                              first_fs,
+            self.calculate_template_splits()
+            first_template = (self.glitch_frequency,
+                              self.timeperiod,
+                              self.fs_template,
                               self.frac_1,
                               self.frac_2,
                               self.frac_3,
@@ -174,21 +141,23 @@ class GenerateTemplateBank:
         while N_templates < self.maximum_templates:
             print('Number of templates in bank: ', len(self.template_bank))
 
-            new_template = (np.random.uniform(low=self.fringe_frequency_lower, high=self.fringe_frequency_upper),
-                            np.random.uniform(low=self.timeperiod_lower, high=self.timeperiod_upper))
-            new_ts = self.generate_template(new_template[0], new_template[1])
-            new_fs = make_frequency_series(new_ts)
-            new_template = (new_template[0], new_template[1], new_fs)
+            self.glitch_frequency = np.random.uniform(low=self.glitch_frequency_lower,
+                                                    high=self.glitch_frequency_upper)
+            self.timeperiod = np.random.uniform(low=self.timeperiod_lower,
+                                                high=self.timeperiod_upper)
+            self.generate_template()
+            new_template = (self.glitch_frequency,
+                            self.timeperiod,
+                            self.fs_template)
             total_tried += 1
 
             if self.match_template_with_bank(new_template):
                 since_template_added = 0
                 if self.find_template_splits is True:
-                    self.calculate_template_splits(new_template[0],
-                                                   new_template[1])
-                    new_template = (new_template[0],
-                                    new_template[1],
-                                    new_fs,
+                    self.calculate_template_splits()
+                    new_template = (self.glitch_frequency,
+                                    self.timeperiod,
+                                    self.fs_template,
                                     self.frac_1,
                                     self.frac_2,
                                     self.frac_3,
